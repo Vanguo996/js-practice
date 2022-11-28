4703df7d0835f558ceb88dd430e265dc834b05b1
vanguo996 <guowang939@gmail.com>
2022-11-28 22:10:49 +0800

    finish the 02-noteApp

--- 02-notes/app.js
+++ 02-notes/app.js
@@ -1,11 +1,7 @@
 //querySelector选择的是静态集合，而getXXXbyXXX是动态集合
 //只要经过一次查找，使用get
+
 //如果需要多级查找则选择querySelector
-// const notesEl = document.querySelector('.notes');
-// const editBtn = document.querySelector('.edit');
-// const deleteBtn = document.querySelector('.delete');
-// const main = notesEl.querySelector('.main');
-// const textArea = notesEl.querySelector('textarea');
 const addBtn = document.getElementById('add');
 
 const notes = JSON.parse(localStorage.getItem('notes'));
@@ -14,7 +10,6 @@ if (notes) {
    notes.forEach(note => {
         addNewNote(note);
    });
-
 }
 
 addBtn.addEventListener('click', () => {
@@ -22,6 +17,7 @@ addBtn.addEventListener('click', () => {
 });
 
 function addNewNote(text = "") {
+    console.log(text);
     const note = document.createElement('div');
     note.classList.add('note');
     note.innerHTML = `
@@ -32,11 +28,11 @@ function addNewNote(text = "") {
                 <button class="delete">
                     <i class="fas fa-trash-alt"></i></button>
             </div>
-            <div class="main hidden"></div>
-            <textarea></textarea>
+            <div class="main ${text ? '': 'hidden'}"></div>
+            <textarea class="${text ? 'hidden': ''}"></textarea>
         </div>
     `;
-
+    
 
     const editBtn = note.querySelector('.edit');
     const deleteBtn = note.querySelector('.delete');
@@ -44,13 +40,19 @@ function addNewNote(text = "") {
     const textArea = note.querySelector('textarea');
 
     textArea.value = text;
+    
+    //修复第一次刷新页面 无法显示文本问题
+    main.innerHTML = marked.parse(text);
+
 
     editBtn.addEventListener('click', () => {
-        //消除hidden类
+        // 切换hidden
         main.classList.toggle('hidden');
         textArea.classList.toggle('hidden');
-    });
+        textArea.focus();
+    }); 
 
+    // 删除笔记本
     deleteBtn.addEventListener('click', () => {
         note.remove();
     });
@@ -67,9 +69,9 @@ function addNewNote(text = "") {
 }
 
 function updateLS()  {
-    const notesText = document.querySelectorAll('.textarea');
+    const notesText = document.querySelectorAll('textarea');
     const notes = [];
-
+    // 将输入的值存放在notes数组中，
     notesText.forEach(note => notes.push(note.value));
 
     localStorage.setItem('notes',JSON.stringify(notes));
